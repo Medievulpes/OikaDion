05ccb4479b5c2780b2a6ffc331578b75fb530d7f
Medievulpe <91827802+Medievulpe@users.noreply.github.com>
2022-06-11 14:16:15 +0800

    v1.1

--- js/flexbox.js
+++ js/flexbox.js
@@ -1,27 +1,29 @@
-$(window).on("load", function() {
+$(window).on("load", function () {
   //console.log(view);
-  $(".o-card_headerHeroImg").each(function() {
+  $(".o-card_headerHeroImg").each(function () {
     var bg = $(this).attr("data-image");
     $(this).css({ "background-image": "url(" + bg + ")" });
   });
 
-  
-  $(document).on("click touch", ".o-card-headerList li", function(e) {
+
+  $(document).on("click touch", ".o-card-headerList li", function (e) {
     e.stopPropagation();
     return false;
   });
-  
-  
-  $('.o-card_paragraph').each(function(){
+
+
+  $('.o-card_paragraph').each(function () {
     var _this = $(this);
     var pHeight = _this.height();
     console.log(pHeight);
     _this.after('<span class="a-more"></span>')
   });
-  
-  $(document).on( 'click' , '.a-more' , function(){
+
+  $(document).on('click', '.a-more', function () {
     $(this).prev('p').toggleClass('isToggle');
     $(this).toggleClass('isActive');
   });
-  
+
 });
+
+
